30030b67871ac85ad188a96165e990c7bd3875b1
Vladislav <voreoneks@gmail.com>
2021-10-27 00:28:54 +0300

    Я уже не понимаю что происходит, но, кажется, я на правильном пути...

--- release/views.py
+++ release/views.py
@@ -13,10 +13,82 @@ from google_sheets import Sheet
 from .forms import *
 from .models import *
 
+def release_to_sheet(user):
+    date_time = str(datetime.now())
+    release_sheet = Sheet('A3:AM3')
+
+    main_info = MainInfo.objects.get(user_id = user.id)
+    main_info_dict = model_to_dict(main_info)
+    content_type = main_info_dict['content_type']
+    audio = Audio.objects.filter(user_id = user.id)
+    audio_tuple_dict = tuple(model_to_dict(item) for item in audio)
+    video = Video.objects.get(user_id = user.id)
+    video_dict = model_to_dict(video)
+    release_values = tuple()
+
+    main_info_values = (
+        date_time, main_info_dict['name'], main_info_dict['content_type'], main_info_dict['phone_number'], main_info_dict['email'], main_info_dict['is_update_photo'], main_info_dict['photo_link']
+    )
+    release_values += main_info_values
+
+    try:
+        audio_values = (
+            audio_tuple_dict[0]['songers'], audio_tuple_dict[0]['song_title'], audio_tuple_dict[0]['album_title'], audio_tuple_dict[0]['feat'], audio_tuple_dict[0]['genre'], audio_tuple_dict[0]['fio_songer'], audio_tuple_dict[0]['words_author'], audio_tuple_dict[0]['music_author'], audio_tuple_dict[0]['owner_citizenship'], audio_tuple_dict[0]['record_country'], audio_tuple_dict[0]['timing'], audio_tuple_dict[0]['song_preview'], audio_tuple_dict[0]['lexis'], '', '', audio_tuple_dict[0]['audio_link'], '', audio_tuple_dict[0]['clean_link'], '', audio_tuple_dict[0]['release_year']
+        )
+    except:
+        audio_values = tuple('' for i in range(20))
+    
+    release_values += audio_values
+
+    try:
+        video_values = (
+            video_dict['songers'], video_dict['video_title'], video_dict['feat'], video_dict['words_author'], video_dict['music_author'], video_dict['lexis'], video_dict['director'], video_dict['timing'], video_dict['release_year'], video_dict['video_link'], '', video_dict['production_country']
+        )
+    except:
+        release_values = tuple('' for i in range(12))
+        
+    release_values += video_values
+
+    release_data = {
+            'range': 'A3:AM3',
+            'majorDimension': 'ROWS',
+            'values': [release_values,]
+        }
+    release_sheet.append(release_data)
+
+    try:
+        add_audio_sheet = Sheet('A3:AM3')
+        add_audio_values = []
+        for i in range(1, len(audio_tuple_dict)):
+            add_audio_values.append(tuple(('', '',  '',  '',  '',  '',  '', audio_tuple_dict[i]['songers'], audio_tuple_dict[i]['song_title'], audio_tuple_dict[i]['album_title'], audio_tuple_dict[i]['feat'], audio_tuple_dict[i]['genre'], audio_tuple_dict[i]['fio_songer'], audio_tuple_dict[i]['words_author'], audio_tuple_dict[i]['music_author'], audio_tuple_dict[i]['owner_citizenship'], audio_tuple_dict[i]['record_country'], audio_tuple_dict[i]['timing'], audio_tuple_dict[i]['song_preview'], audio_tuple_dict[i]['lexis'], '', '', audio_tuple_dict[i]['audio_link'], '', audio_tuple_dict[i]['clean_link'], '', audio_tuple_dict[i]['release_year'])))
+
+        add_audio_data = {
+            'range': 'A3:AM3',
+            'majorDimension': 'ROWS',
+            'values': add_audio_values
+            }
+        add_audio_sheet.append(add_audio_data)
+    except:
+        pass
+
+    spaces = Sheet('A3:AM3')
+    spaces_values = [tuple('.' for i in range(39))]
+    spaces_data = {
+        'range': 'A3:AM3',
+        'majorDimension': 'ROWS',
+        'values': spaces_values
+    }
+    spaces.append(spaces_data)
+    main_info.delete()
+    for item in audio:
+        item.delete()
+    video.delete()
+
 
 class MainInfoView(LoginRequiredMixin, FormView):
     template_name = 'release/release.html'
     form_class = MainInfoForm
+    form_title = 'Главная информация'
 
     def get(self, request, *args, **kwargs):
         user = User.objects.get(username = request.user)
@@ -28,7 +100,7 @@ class MainInfoView(LoginRequiredMixin, FormView):
             form = self.form_class(initial = data)
         except:
             form = self.form_class(initial = {'user': user})
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {'form': form, 'form_title': self.form_title})
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(data = request.POST, files = request.FILES)
@@ -47,12 +119,13 @@ class MainInfoView(LoginRequiredMixin, FormView):
                 self.next = 'r_video'
             return HttpResponseRedirect(reverse(self.next))
         else:
-            return render(request, self.template_name, {'form': form})
+            return render(request, self.template_name, {'form': form, 'form_title': self.form_title})
 
 
 class AudioView(LoginRequiredMixin, FormView):
     template_name = 'release/audio.html'
     form_class = AudioForm
+    form_title = 'Аудио'
     fields_for_button = ['songers', 'song_title', 'album_title', 'feat', 'genre', 'fio_songer', 'words_author', 'music_author', 'owner_citizenship', 'record_country', 'timing', 'song_preview', 'lexis', 'audio_link', 'clean_link', 'release_year', 'add_video',]
 
     def get(self, request, *args, **kwargs):
@@ -72,7 +145,7 @@ class AudioView(LoginRequiredMixin, FormView):
             for num in range(num_songs):
                 data['form-' + str(num) + '-user'] = user.id
         formset = audio_formset(data)
-        return render(request, self.template_name, {'button': self.fields_for_button, 'formset': formset})
+        return render(request, self.template_name, {'button': self.fields_for_button, 'formset': formset, 'form_title': self.form_title})
 
     def post(self, request, *args, **kwargs):
         user = User.objects.get(username = request.user)
@@ -89,11 +162,12 @@ class AudioView(LoginRequiredMixin, FormView):
             for form in formset:
                 form.save()
             if add_video == 'NO':
+                release_to_sheet(user)
                 return HttpResponseRedirect(reverse('r_success'))
             else:
                 return HttpResponseRedirect(reverse('r_video'))
         else:
-            return render(request, self.template_name, {'button': self.fields_for_button, 'formset': formset})
+            return render(request, self.template_name, {'button': self.fields_for_button, 'formset': formset, 'form_title': self.form_title})
 
 
 class VideoView(LoginRequiredMixin, FormView):
@@ -123,60 +197,7 @@ class VideoView(LoginRequiredMixin, FormView):
             except:
                 pass
             form.save()
-
-            date_time = str(datetime.now())
-            main_info_sheet = Sheet('A3:AM3')
-
-            main_info = MainInfo.objects.get(user_id = user.id)
-            main_info_dict = model_to_dict(main_info)
-            content_type = main_info_dict['content_type']
-            audio = Audio.objects.filter(user_id = user.id)
-            audio_tuple_dict = tuple(model_to_dict(item) for item in audio)
-            video = Video.objects.get(user_id = user.id)
-            video_dict = model_to_dict(video)
-
-            main_info_values = [
-                (date_time, main_info_dict['name'], main_info_dict['content_type'], main_info_dict['phone_number'], main_info_dict['email'], main_info_dict['is_update_photo'], main_info_dict['photo_link'], audio_tuple_dict[0]['songers'], audio_tuple_dict[0]['song_title'], audio_tuple_dict[0]['album_title'], audio_tuple_dict[0]['feat'], audio_tuple_dict[0]['genre'], audio_tuple_dict[0]['fio_songer'], audio_tuple_dict[0]['words_author'], audio_tuple_dict[0]['music_author'], audio_tuple_dict[0]['owner_citizenship'], audio_tuple_dict[0]['record_country'], audio_tuple_dict[0]['timing'], audio_tuple_dict[0]['song_preview'], audio_tuple_dict[0]['lexis'], '', '', audio_tuple_dict[0]['audio_link'], '', audio_tuple_dict[0]['clean_link'], '', audio_tuple_dict[0]['release_year'], video_dict['songers'], video_dict['video_title'], video_dict['feat'], video_dict['words_author'], video_dict['music_author'], video_dict['lexis'], video_dict['director'], video_dict['timing'], video_dict['release_year'], video_dict['video_link'], '', video_dict['production_country']),
-                ]
-
-            main_info_data = {
-                    'range': 'A3:AM3',
-                    'majorDimension': 'ROWS',
-                    'values': main_info_values
-                }
-            main_info_sheet.append(main_info_data)
-
-
-            add_audio_sheet = Sheet('A3:AM3')
-            add_audio_values = []
-            for i in range(1, len(audio_tuple_dict)):
-                add_audio_values.append(tuple(('', '',  '',  '',  '',  '',  '', audio_tuple_dict[i]['songers'], audio_tuple_dict[i]['song_title'], audio_tuple_dict[i]['album_title'], audio_tuple_dict[i]['feat'], audio_tuple_dict[i]['genre'], audio_tuple_dict[i]['fio_songer'], audio_tuple_dict[i]['words_author'], audio_tuple_dict[i]['music_author'], audio_tuple_dict[i]['owner_citizenship'], audio_tuple_dict[i]['record_country'], audio_tuple_dict[i]['timing'], audio_tuple_dict[i]['song_preview'], audio_tuple_dict[i]['lexis'], '', '', audio_tuple_dict[i]['audio_link'], '', audio_tuple_dict[i]['clean_link'], '', audio_tuple_dict[i]['release_year'])))
-
-            add_audio_data = {
-                'range': 'A3:AM3',
-                'majorDimension': 'ROWS',
-                'values': add_audio_values
-                }
-            add_audio_sheet.append(add_audio_data)
-
-            spaces = Sheet('A3:AM3')
-            spaces_values = [tuple('.' for i in range(39))]
-            spaces_data = {
-                'range': 'A3:AM3',
-                'majorDimension': 'ROWS',
-                'values': spaces_values
-            }
-            spaces.append(spaces_data)
-
-
-            # if len(audio_tuple_dict) > 1:
-            #     audio_values = []
-            #     fields = (audio_tuple_dict[i]['songers'], audio_tuple_dict[i]['song_title'], audio_tuple_dict[i]['feat'], audio_tuple_dict[i]['genre'], audio_tuple_dict[i]['fio_songer'], audio_tuple_dict[i]['words_author'], audio_tuple_dict[i]['music_author'], audio_tuple_dict[i]['timing'], audio_tuple_dict[i]['song_preview'], audio_tuple_dict[i]['lexis'], '', '', audio_tuple_dict[i]['audio_link'], '', audio_tuple_dict[i]['clean_link'], '', audio_tuple_dict[i]['release_year']) 
-            #     for i in range(1, len(audio_tuple_dict)):
-            #         audio_values.append()                    
-
-
-    
+            release_to_sheet(user)
             return HttpResponseRedirect(reverse('r_success'))
         else:
             return render(request, self.template_name, {'form': form})
